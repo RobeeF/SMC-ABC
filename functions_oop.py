14397a192d011e0cc336d1cee33977d5c10baf74
Robin Fuchs <robin.fuchs92@gmail.com>
2019-01-14 23:13:54 +0100

    update(functions, mains): last updates for instances add docstrings and comments

--- functions_oop.py
+++ functions_oop.py
@@ -30,7 +30,10 @@ class SMC_ABC(object):
     def generate_sample_from_clusters(self, clusters_sizes, nb_of_clusters_of_that_size): # need to shuffle ?
         '''Generates a sample from the types in the population and the number of people that are of that type 
         clusters_sizes: (array-like)  How many people in each cluster (a cluster = people with the same type)
-        nb_of_clusters_of_that_size: (array-like) How many clusters have [clusters_sizes] people in it'''    
+        nb_of_clusters_of_that_size: (array-like) How many clusters have [clusters_sizes] people in it
+        -----------------------------------------------------------------------------------------------
+        returns: (array-like) an array with the types of tuberculosis presents in the sample
+        '''    
         # Initialisation
         current_group_index = 0 # Assign numbers to each cluster 
         sample = [] # Store the samples generated
@@ -42,25 +45,33 @@ class SMC_ABC(object):
         return np.array(sample)
     
     
-    def compute_eta(self, pop):
-        ''' Compute eta p9 of the paper'''
-        gi, ni = np.unique(pop, return_counts=True) 
+    def compute_eta(self, sample):
+        ''' Compute eta p9 of the paper
+        sample (array-like): The sample for which to compute eta
+        --------------------------------------------------------
+        returns: etas = (the number of different types in the sample, the genes diversity)
+        '''
+        gi, ni = np.unique(sample, return_counts=True) 
         g = len(ni)
         n = np.sum(ni)
         return (g,1-np.square(ni).sum()/n**2)
     
     def compute_rho(self, eta):
-        ''' Compute rho as in the paper 
+        ''' Compute rho as in Tanaka (2006) between self.eta the eta of the tuberculosis data and a given generated sample
         eta (tuple of size 2): a mesure performed on a sample
-        eta (tuple of size 2): a mesure performed on another sample
         n: size of the samples used to compute the etas
-        returns: (float) rho
+        returns: (float) rho a measure of the dissimilarity of the sample
         '''
         return (abs(eta[0] - self.actual_eta[0]) + abs(eta[1] - self.actual_eta[1]))/self.sample_size
     
     
     def compute_prior(self):
         # Maybe the definition of gamma is wrong the paper seems to inverse a and b in the definition of the mean of the gamma
+        ''' 
+        Compute theta the prior for generating the populations 
+        ------------------------------------------------------------------
+        returns: ((1,3)-array) a non-normalized prior for population generation 
+        '''
         phi, xi = gamma.rvs(a=0.1, size=self.N),truncnorm.rvs(a=0, b=10**10, loc=0.198, scale=0.067352, size=self.N)
         tau = uniform.rvs(loc=0,scale=phi, size=self.N)
         theta = np.dstack((phi,tau,xi))[0]
@@ -68,6 +79,14 @@ class SMC_ABC(object):
     
     @jit(parallel=True)
     def samples_and_etas_from_pop(self, thetas):
+        ''' 
+        From a given prior (thetas) draw a sample from the generated population 
+        thetas ((number of alive particules, 3)-array) all the priors associated with the alive particules  
+        ---------------------------------------------------------------------------------------------------
+        returns: - samples ((number of alive particules, 3)-array) the samples generated according to the priors
+        - etas: the etas corresponding to the sample
+        - priors_that_generated_population: ((number of alive particules,3)-array) the priors that succeeded in generating a prior
+        '''
         samples = []
         etas = []    
         has_pops_generation_failed = []
@@ -85,12 +104,14 @@ class SMC_ABC(object):
     
     @jit
     def simulate_population(self, theta, verbose=True):
-        ''' Simulate the population as presented in the paper 
-        N: (int) size of the particle to simulate
-        theta: (array-like) the theta parameter (improve def) 
-        verbose: if True then the algorithm prints intermediate results
+        ''' 
+        Simulate the population as presented in the paper 
+        theta (array-like): the prior to simulate the population from
+        verbose (boolean): if True then the algorithm prints intermediate results
+        -------------------------------------------------------------------------------
+        returns: (array-like) particle of size n, the values in the array are the type of each individual belonging in the population 
+        '''    
         
-        returns: (array-like) particle of size n, the values in the array are the type of each individual belonging in the population '''    
         X = np.ones(1, 'int')
         G = 1 
 
@@ -129,29 +150,43 @@ class SMC_ABC(object):
                 Gi,Ni = Gi[1:], Ni[1:]
             return self.generate_sample_from_clusters(Ni.astype(int),Gi.astype(int)), fail_to_gen_pop
 
-
-        
-    
-    
-    #==========================================================================================================
-    # SMC Sampler
-    #==========================================================================================================
     def compute_ess(self, Wn): 
+        ''' 
+        Compute the effective sample size of a collection of samples
+        Wn (arry-like): The weights associated to the collection of samples
+        -------------------------------------------------------------------
+        returns ESS (int): The Effective sample size
+        '''
         return (np.square(Wn).sum())**(-1)
     
     def compute_Wn(self, prev_weights, prev_epsilon, current_epsilon, gen_sample_etas):
-
+        '''
+        Compute the new weights according to Del Moral & al. (2006)
+        prev_weights (array-like): the weights of the previous iteration
+        prev_epsilon (float): The tolerance level of the last iteration
+        current_epsilon (float): the epsilon of the current iteration
+        gen_sample_etas (array-like): the etas associated with the previously generated samples  
+        ----------------------------------------------------------------
+        returns (array-like): the new weights
+        '''
         new_weights =  prev_weights*np.array([int(self.compute_rho(eta)<current_epsilon) for eta in gen_sample_etas])/np.array([int(self.compute_rho(eta)<prev_epsilon) for eta in gen_sample_etas])
         new_weights = np.nan_to_num(new_weights) # For the weights divided by 0
         new_weights = new_weights/new_weights.sum() if new_weights.sum()!=0 else new_weights# Renomalise the weights 
         return new_weights
     
-    def find_epsilon_n(self, prev_epsilon, prev_weights, gen_sample_etas, prev_ess):
-        ''' Returns the next epsilon such that ESS(new_epsilon) is the closest to ESS(prev_epsilon)
+    def find_epsilon_n(self, prev_epsilon, prev_weights, gen_sample_etas, prev_ess): # As to be replaced by the bissection method
+        ''' 
+        Returns the next epsilon such that ESS(new_epsilon) is the closest to alpha*ESS(prev_epsilon)
+        prev_epsilon (float): The tolerance level of the last iteration
+        prev_weights (array-like): the weights of the previous iteration
+        gen_sample_etas (array-like): the etas associated with the previously generated samples  
+        prev_ess (float): the effective sample size of the previous iteration
+        ------------------------------------------------------------------------------
+        returns (float) the epsilon closest to the true solution of ESS(new_epsilon) - alpha*ESS(prev_epsilon) = 0
         '''
         
         SIZE_GRID_EPSILON = 10
-        new_epsilons = np.linspace(self.e,prev_epsilon, 25 + np.ceil(SIZE_GRID_EPSILON/prev_epsilon))
+        new_epsilons = np.linspace(self.e,prev_epsilon, 25 + np.ceil(SIZE_GRID_EPSILON/prev_epsilon)) # Force the grid to be thin when epsilon is small to foster exploration of this zone
         new_epsilon_ess_dict = {}
         for epsilon in new_epsilons:
             new_weights = self.compute_Wn(prev_weights, prev_epsilon, epsilon, gen_sample_etas)
@@ -165,11 +200,21 @@ class SMC_ABC(object):
         min_value = min(dist_to_alpha_ess.values())
         best_epsilon = min([key for key,val in dist_to_alpha_ess.items() if val == min_value])
         return best_epsilon
+    
         
-    def indiv_MHRW(self, theta, cov, epsilon, prev_gen_sample ,prev_gen_eta):
-        ''' Return the Metropolis Hastings Random Walk Kernel updates of the thetas=(phi, eta, tau) '''
-        # Need to reparametrize theta over the real line ? 
-        new_theta = theta + np.random.multivariate_normal([0,0,0], 2*cov) # Pas sûr que ce soit le bon proposal
+    def indiv_MHRW(self, theta, cov, epsilon, prev_gen_sample, prev_gen_eta):
+        ''' 
+        Return the Metropolis Hastings Random Walk Kernel updates of the thetas=(phi, eta, tau) 
+        theta: (array-like) the prior to simulate the population from
+        cov: ((3,3)-array): The covariance matrix of the thetas previously computed
+        epsilon (float): The tolerance level of the current iteration
+        prev_gen_sample (array-like): A sample previously generated
+        prev_gen_eta (array-like): The eta associated to that sample
+        -------------------------------------------------------------------------
+        returns (array-like): the updated or not theta
+        '''
+        
+        new_theta = theta + np.random.multivariate_normal([0,0,0], ((2.38**2)/3)*cov) # Has dimensionality is 3
         new_theta = new_theta/new_theta.sum() # reparametrization on the real line. Is it the right way to do it ? 
         
         new_pop, fail_to_gen_pop = self.simulate_population(theta, verbose=False)
@@ -195,9 +240,15 @@ class SMC_ABC(object):
     
     @jit
     def sampler(self, full_output=False):
+        ''' 
+        Implementation of the sampler as in Del Moral and al. (2006)
+        full_output (boolean): if False returns just the thetas else returns also the ess, epsilon and samples trajectories through the iterations
+        ----------------------------------------------------------------------------------------------------------
+        returns (see full_output definition)
+        '''
         nb_alive_particles = 0
         
-        # While there are no alive paricules at n=0 we relaunch the step 0
+        # If there are no alive paricules at n=0 we relaunch the step 0
         while nb_alive_particles==0:
             epsilon = []
             ess = [] 
@@ -208,7 +259,7 @@ class SMC_ABC(object):
             
             # Step 0 
             n=0        
-            epsilon.append(500) # Set epsilonO to a quite big number
+            epsilon.append(400) # Set epsilonO to a quite big number
             ess.append(self.N) # Append ESSo
             
             priors = self.compute_prior()
@@ -222,18 +273,13 @@ class SMC_ABC(object):
 
         
         # Step 1
-        while epsilon[n-1]>= self.e:
+        while epsilon[n-1]> self.e:
             n+=1
             print(n)
-            if n>10:
-                break
             
             epsilon.append(self.find_epsilon_n(epsilon[n-1], weights[n-1], etas[n-1], ess[n-1])) # Solve ESS = alpha*ESS
             weights.append(self.compute_Wn(weights[n-1],epsilon[n-1],epsilon[n], etas[n-1]))
             ess.append(self.compute_ess(weights[n]))
-            print('Nb of alive particles', np.count_nonzero(weights[n]))
-            print('ESS is ', ess[n])
-            print('epsilon', epsilon[n])
             
             # Step 2
             if np.ceil(ess[n])<self.N_T:
@@ -254,7 +300,6 @@ class SMC_ABC(object):
                     
             samples, etas_samples, priors_that_generated_pop = self.samples_and_etas_from_pop(thetas_from_MHRW)
             weights[n] = np.full(len(samples),1/len(samples)) # Some particules have died since resampling/step 1
-            print('step 3 weights ', weights[n])
             thetas.append(copy.deepcopy(np.stack(np.array(priors_that_generated_pop))))
             X.append(copy.deepcopy(np.stack(np.array(samples))))
             etas.append(copy.deepcopy(np.stack(np.array(etas_samples))))
--- main.py
+++ main.py
@@ -11,12 +11,14 @@ os.chdir('C:/Users/robin/Documents/GitHub/SMC-ABC') # Change path to run it on y
 from functions_oop import *
  
 import numpy as np
+import pickle
 #from functions import *
 
-N = 50
+N = 1000
 alpha = 0.9
-e = 0.01
-pop_size=10000
+e = 0.0025
+pop_size= 1000 #pop_size= 473
+# while 20 fois
 
 actual_data = np.array([30,23,15,10,8,5,4,3,2,1]), np.array([1,1,1,1,1,2,4,13,20,282]) # Data described in the paper end of p8
 smc_abc = SMC_ABC(actual_data, N, e, pop_size, alpha)
@@ -24,7 +26,21 @@ smc_abc.sampler(True)
 
 output = smc_abc.output
 
-thetas = np.concatenate(np.array(output[0]))
+with open('smc_abc_'+str(N)+'_'+str(e)+'_'+ str(pop_size), 'wb') as fichier:
+    mon_pickler = pickle.Pickler(fichier)
+    mon_pickler.dump(output)
+  
+    
+    
+#=========================================================================================================
+# Analysis
+#=========================================================================================================
+with open('smc_abc_'+str(N)+'_'+str(e)+'_'+ str(pop_size), 'rb') as fichier:
+        mon_depickler = pickle.Unpickler(fichier)
+        output_depickled = mon_depickler.load()
+
+smc_abc.pop_size
+thetas = np.concatenate(np.array(output_depickled[0]))
 phi = thetas[:,0]
 tau =  thetas[:,1]
 xi =  thetas[:,2]
@@ -33,14 +49,55 @@ pd.Series(phi).plot(kind='density')
 pd.Series(tau).plot(kind='density')
 pd.Series(xi).plot(kind='density')
 
+acc_rate = phi - tau
+pd.Series(acc_rate).plot(kind='density')
+
+log2_over_acc_rate = np.log(2)/acc_rate
+pd.Series(log2_over_acc_rate).plot(kind='density')
+
+phi.mean()
+tau.mean()
+xi.mean()
 
 ess = output[1]
 pd.Series(ess).plot()
 
 epsilon = output[2]
-pd.Series(epsilon).plot() 
+pd.Series(epsilon[1:]).plot() 
 
-w = np.ones(4)
-a = np.full(10,0)
-np.concatenate(w,a, axis=1)
+#=========================================================================================================
+# Posterior mean
+#=========================================================================================================
+
+N = 50 
+alpha = 0.9
+e = 0.0025
+pop_size= 473
+
+posterior_means = []
+for i in range(20):
+    print('--------------------------------------------------------------')
+    print('iteration =', i)
+    try:
+        smc_abc = SMC_ABC(actual_data, N, e, pop_size, alpha)
+        smc_abc.sampler(True)
+        thetas = np.concatenate(np.array(smc_abc.output[0]))
+        phi = thetas[:,0]
+        tau =  thetas[:,1]
+        xi =  thetas[:,2]
+        posterior_means.append(np.array([phi.mean(), tau.mean(),xi.mean()]))
+    except RuntimeError:
+        print(RuntimeError)
+        pass
+
+
+
+post_means = np.stack(np.array(posterior_means))
+phi_mean = post_means[:,0]
+tau_mean =  post_means[:,1]
+xi_mean =  post_means[:,2]
+
+pd.Series(phi_mean).plot(kind='density')
+pd.Series(tau_mean).plot(kind='density')
+pd.Series(xi_mean).plot(kind='density')
 
