fix(functions), update(main, functions): several bugs in functions about weight computation, added a stop when the population could not have been generated whereas the number of iterations is too large: typically more than 7 times N

functions_oop.py
@@ -20,7 +20,6 @@ class SMC_ABC(object):
         self.data = self.generate_sample_from_clusters(data[0], data[1])
         self.actual_eta = self.compute_eta(self.data)
         self.N = N
-        self.priors = self.compute_prior()
         self.e = e
         self.pop_size = pop_size
         self.sample_size = len(self.data)
@@ -63,22 +62,27 @@ class SMC_ABC(object):
     
     def compute_prior(self):
         # Maybe the definition of gamma is wrong the paper seems to inverse a and b in the definition of the mean of the gamma
-        # Put mean of the gamma to 0.5 instead of 0.1 because too long to run otherwise
-        phi, xi = gamma.rvs(a=0.5, size=self.N),truncnorm.rvs(a=0, b=10**10, loc=0.198, scale=0.067352, size=self.N)
+        phi, xi = gamma.rvs(a=0.1, size=self.N),truncnorm.rvs(a=0, b=10**10, loc=0.198, scale=0.067352, size=self.N)
         tau = uniform.rvs(loc=0,scale=phi, size=self.N)
         theta = np.dstack((phi,tau,xi))[0]
         return theta
     
     @jit(parallel=True)
-    def samples_and_etas_from_pop(self, priors):
+    def samples_and_etas_from_pop(self, thetas):
         samples = []
-        etas = []
-        for theta in priors:
-            pop = self.simulate_population(theta, True)
-            samples.append(np.random.choice(pop, self.sample_size, replace=False))
-            etas.append(self.compute_eta(pop))
-            
-        return samples, etas
+        etas = []    
+        has_pops_generation_failed = []
+        priors_that_generated_population = []
+        
+        for theta in thetas:
+            pop, has_pop_generation_failed = self.simulate_population(theta, verbose=False)
+            if has_pop_generation_failed==False:
+                samples.append(np.random.choice(pop, self.sample_size, replace=False))
+                etas.append(self.compute_eta(pop))
+                has_pops_generation_failed.append(True)
+                priors_that_generated_population.append(theta)
+
+        return samples, etas, priors_that_generated_population
     
     @jit
     def simulate_population(self, theta, verbose=True):
@@ -92,42 +96,43 @@ class SMC_ABC(object):
         X = np.ones(1, 'int')
         G = 1 
 
-        fails = 0
-        t=1 
+        t=1
+        fail_to_gen_pop = False
         
         while X.sum()<self.pop_size: 
-            if X.sum()>0:  
-                t+=1
-                event_picked = multinomial(n=1, pvals=theta/theta.sum())
-                type_picked = multinomial(n=1, pvals=X/X.sum()) # Pas propre besoin d'une conversion
-                type_picked_index = np.where(type_picked==1)[0][0]
-                
-                if (event_picked == np.array([1,0,0])).all():
-                    X[type_picked_index] += 1 
-                            
-                elif (event_picked == np.array([0,1,0])).all(): # Death
-                    X[type_picked_index] -= 1 
+            if verbose:
+                print(X.sum())
+            t+=1
+            event_picked = multinomial(n=1, pvals=theta/theta.sum())
+            type_picked = multinomial(n=1, pvals=X/X.sum()) # Pas propre besoin d'une conversion
+            type_picked_index = np.where(type_picked==1)[0][0]
             
-                else: # Mutation
-                    X[type_picked_index] -= 1 
-                    X =np.append(X, 1)
-                    G+=1
-    
-                if verbose:
-                    print(X.sum())
-            else: # If all types have died we relaunch the algorithm (as prescribed in Tanaka (2006) p4)
-                t= 1
-                X,G = np.ones(1),1
-                fails+=1
-                if verbose:
-                    print('The generation of the population has failed ', fails,' times')
+            if (event_picked == np.array([1,0,0])).all():
+                X[type_picked_index] += 1 
+                        
+            elif (event_picked == np.array([0,1,0])).all(): # Death
+                X[type_picked_index] -= 1 
         
-        # Turn X into a sample of pop = X$
-        Gi, Ni = np.unique(X, return_counts=True) # Count size of each group and how many groups have this size 
-        if Gi[0]==0: # We don't care about the types that have 0 people
-            Gi,Ni = Gi[1:], Ni[1:]
+            else: # Mutation
+                X[type_picked_index] -= 1 
+                X =np.append(X, 1)
+                G+=1
+                
+            if X.sum()==0 or t>=7*self.pop_size:
+                fail_to_gen_pop=True
+                break
+            
+        if fail_to_gen_pop:
+            return np.full(self.N,np.nan), fail_to_gen_pop
+        else:
+            # Turn X into a sample of pop = X$
+            Gi, Ni = np.unique(X, return_counts=True) # Count size of each group and how many groups have this size 
+            if Gi[0]==0: # We don't care about the types that have 0 people
+                Gi,Ni = Gi[1:], Ni[1:]
+            return self.generate_sample_from_clusters(Ni.astype(int),Gi.astype(int)), fail_to_gen_pop
+
+
         
-        return self.generate_sample_from_clusters(Ni.astype(int),Gi.astype(int))
     
     
     #==========================================================================================================
@@ -137,6 +142,7 @@ class SMC_ABC(object):
         return (np.square(Wn).sum())**(-1)
     
     def compute_Wn(self, prev_weights, prev_epsilon, current_epsilon, gen_sample_etas):
+
         new_weights =  prev_weights*np.array([int(self.compute_rho(eta)<current_epsilon) for eta in gen_sample_etas])/np.array([int(self.compute_rho(eta)<prev_epsilon) for eta in gen_sample_etas])
         new_weights = np.nan_to_num(new_weights) # For the weights divided by 0
         new_weights = new_weights/new_weights.sum() if new_weights.sum()!=0 else new_weights# Renomalise the weights 
@@ -146,6 +152,7 @@ class SMC_ABC(object):
     def find_epsilon_n(self, prev_epsilon, prev_weights, gen_sample_etas, prev_ess):
         ''' Returns the next epsilon such that ESS(new_epsilon) is the closest to ESS(prev_epsilon)
         '''
+        
         SIZE_GRID_EPSILON = 10
         new_epsilons = np.linspace(self.e,prev_epsilon,5 + np.ceil(SIZE_GRID_EPSILON/prev_epsilon))
         new_epsilon_ess_dict = {}
@@ -168,87 +175,94 @@ class SMC_ABC(object):
         new_theta = theta + np.random.multivariate_normal([0,0,0], 2*cov) # Pas sûr que ce soit le bon proposal
         new_theta = new_theta/new_theta.sum() # reparametrization on the real line. Is it the right way to do it ? 
         
-        new_pop = self.simulate_population(theta, False)
-        new_sample = np.random.choice(new_pop, self.sample_size, replace=True)
-        new_gen_eta = self.compute_eta(new_sample)
+        new_pop, fail_to_gen_pop = self.simulate_population(theta, verbose=False)
+        if fail_to_gen_pop==False:
+            new_sample = np.random.choice(new_pop, self.sample_size, replace=True)
+            new_gen_eta = self.compute_eta(new_sample)
+            
+            indicatrices_ratio = int(self.compute_rho(new_gen_eta)<epsilon)/int(self.compute_rho(prev_gen_eta)<epsilon)
+            proposal_ratio = 1 # random walk proposal is symetric then the ratio of q(theta*,theta)/q(theta,theta*)=1
+            new_priors = np.array([gamma.pdf(x=new_theta[0],a=0.1),uniform.pdf(x=new_theta[1],loc=0,scale=theta[0]),truncnorm.pdf(x=new_theta[2],a=0, b=10**10, loc=0.198, scale=0.067352)])
+            old_priors = np.array([gamma.pdf(x=theta[0],a=0.1),uniform.pdf(x=theta[1],loc=0,scale=theta[0]),truncnorm.pdf(x=theta[2],a=0, b=10**10, loc=0.198, scale=0.067352)])
         
-        indicatrices_ratio = int(self.compute_rho(new_gen_eta)<epsilon)/int(self.compute_rho(prev_gen_eta)<epsilon)
-        proposal_ratio = 1 # random walk proposal is symetric then the ratio of q(theta*,theta)/q(theta,theta*)=1
-        new_priors = np.array([gamma.pdf(x=new_theta[0],a=0.1),uniform.pdf(x=new_theta[1],loc=0,scale=theta[0]),truncnorm.pdf(x=new_theta[2],a=0, b=10**10, loc=0.198, scale=0.067352)])
-        old_priors = np.array([gamma.pdf(x=theta[0],a=0.1),uniform.pdf(x=theta[1],loc=0,scale=theta[0]),truncnorm.pdf(x=theta[2],a=0, b=10**10, loc=0.198, scale=0.067352)])
-    
-        acceptance_probas = np.minimum(np.ones(len(theta)),indicatrices_ratio*proposal_ratio*new_priors/old_priors)
-        unif_draws = uniform.rvs(size=len(theta))
-        
-        new_theta_accepted = unif_draws<=acceptance_probas
-        final_theta = np.where(new_theta_accepted,new_theta, theta)
+            acceptance_probas = np.minimum(np.ones(len(theta)),indicatrices_ratio*proposal_ratio*new_priors/old_priors)
+            unif_draws = uniform.rvs(size=len(theta))
+            
+            new_theta_accepted = unif_draws<=acceptance_probas
+            final_theta = np.where(new_theta_accepted,new_theta, theta)
+            
+        else: 
+            final_theta = theta
 
-        return final_theta, acceptance_probas # Check if final_theta<1 for all theta
+        return final_theta # Might return acceptance probas
     
     @jit
     def sampler(self, full_output=False):
+        nb_alive_particles = 0
         
-        epsilon = []
-        ess = [] 
-        weights = []
-        thetas = []
-        X = [] # Each element of this array is the N samples at time n
-        acceptance_probas = []
-        samples = []
-        etas = []
-        
-        # Step 0 
-        n=0        
-        epsilon.append(10**6) # Set epsilonO to a big number
-        ess.append(self.N) # Append ESSo
-        weights.append(np.full(self.N,1/self.N)) # Append Wo
-        thetas.append(self.priors)
-        
-        samples, etas = self.samples_and_etas_from_pop(thetas[0])
-
-        X.append(samples)
+        # While there are no alive paricules at n=0 we relaunch the step 0
+        while nb_alive_particles==0:
+            epsilon = []
+            ess = [] 
+            weights = []
+            thetas = []
+            X = [] # Each element of this array is the N samples at time n
+            etas = []
+            
+            # Step 0 
+            n=0        
+            epsilon.append(10**6) # Set epsilonO to a big number
+            ess.append(self.N) # Append ESSo
+            
+            priors = self.compute_prior()
+            samples, etas_samples, priors_that_generated_pop = self.samples_and_etas_from_pop(priors)
+            nb_alive_particles = len(samples)
+            
+        weights.append(np.full(len(samples),1/len(samples))) # W_O  
+        thetas.append(np.stack(np.array(priors_that_generated_pop), axis=0))      
+        X.append(np.stack(np.array(samples),axis=0))
+        etas.append(np.stack(np.array(etas_samples),axis=0))
 
         
         # Step 1
         while (n<self.T) and (epsilon[n-1]>= self.e):
             n+=1
             print(n)
-
-            epsilon.append(self.find_epsilon_n(epsilon[n-1], weights[n-1], etas, ess[n-1])) # Solve ESS = alpha*ESS
-            weights.append(self.compute_Wn(weights[n-1],epsilon[n-1],epsilon[n], etas))
+            
+            epsilon.append(self.find_epsilon_n(epsilon[n-1], weights[n-1], etas[n-1], ess[n-1])) # Solve ESS = alpha*ESS
+            weights.append(self.compute_Wn(weights[n-1],epsilon[n-1],epsilon[n], etas[n-1]))
             ess.append(self.compute_ess(weights[n]))
             
             # Step 2
             if ess[n]<self.N_T:
                 print('resampling')
-                indices = rs.systematic(weights[n])
+                indices = rs.systematic(W=weights[n],M=self.N)
                 thetas[n-1] = copy.deepcopy(thetas[n-1][indices])
                 X[n-1] = copy.deepcopy(X[n-1][indices])
+                etas[n-1] = copy.deepcopy(etas[n-1][indices])
                 weights[n] = np.full(self.N,1/self.N)
                     
             # Step 3
             print('Kernel update')
             cov_theta = np.cov(np.array(thetas[n-1]), rowvar=False)            
-            thetas_from_MHRW = []
-            ac_proba_from_MHRW = []
-            for i in prange(self.N):
+            thetas_from_MHRW = []            
+            for i in prange(len(weights[n])):
                 if weights[n][i]>0:
-                    MHRW_results = self.indiv_MHRW(thetas[n-1][i], cov_theta, epsilon[n-1], X[n-1][i], etas[i])
-                    thetas_from_MHRW.append(np.stack(MHRW_results[0]))
-                    ac_proba_from_MHRW.append(MHRW_results[1])
+                    thetas_from_MHRW.append(np.stack(self.indiv_MHRW(thetas[n-1][i], cov_theta, epsilon[n-1], X[n-1][i], etas[n-1][i])))
+                    
+            samples, etas_samples, priors_that_generated_pop = self.samples_and_etas_from_pop(thetas_from_MHRW)
+            weights[n] = np.full(len(samples),1/len(samples)) # Some particules have died since resampling/step 1
             
-            samples, etas = self.samples_and_etas_from_pop(thetas_from_MHRW)
-                        
-            thetas.append(copy.deepcopy(np.stack(np.array(thetas_from_MHRW))))
+            thetas.append(copy.deepcopy(np.stack(np.array(priors_that_generated_pop))))
             X.append(copy.deepcopy(np.stack(np.array(samples))))
-            acceptance_probas.append(copy.deepcopy(np.stack(np.array(ac_proba_from_MHRW))))
-            etas = np.array(etas)
-
-        if full_output: 
-            self.output = np.stack(thetas)[1:], np.stack(acceptance_probas), ess, epsilon
-            return np.stack(thetas)[1:], np.stack(acceptance_probas), ess, epsilon
-        
+            etas.append(copy.deepcopy(np.stack(np.array(etas_samples))))
+            print('end of the Kernel update')
+            
+        if full_output:
+            self.output = thetas, ess, epsilon
+            return thetas, ess, epsilon
+                
         else:
-            self.output = np.stack(thetas)[1:]
-            return np.stack(thetas)[1:] # [1:] to not return the prior draw of n=0
-
+            self.output = thetas
+            return thetas 
+        

main.py
@@ -13,11 +13,11 @@ from functions_oop import *
 import numpy as np
 #from functions import *
 
-N = 3
+N = 100
 alpha = 0.9
 e = 0.0045
-pop_size=1000
-T=10
+pop_size=10000
+T=30
 
 actual_data = np.array([30,23,15,10,8,5,4,3,2,1]), np.array([1,1,1,1,1,2,4,13,20,282]) # Data described in the paper end of p8
 smc_abc = SMC_ABC(actual_data, N, e, pop_size, T, alpha)
@@ -25,25 +25,23 @@ smc_abc.sampler(True)
 
 output = smc_abc.output
 
-phi = np.concatenate(output[0][:,:,0].reshape(-1,1))
-tau =  np.concatenate(output[0][:,:,1].reshape(-1,1))
-xi =  np.concatenate(output[0][:,:,2].reshape(-1,1))
+thetas = np.concatenate(np.array(output[0]))
+phi = thetas[:,0]
+tau =  thetas[:,1]
+xi =  thetas[:,2]
 
 pd.Series(phi).plot(kind='density')
 pd.Series(tau).plot(kind='density')
 pd.Series(xi).plot(kind='density')
 
 
-phi_acceptance = output[1][:,:,0].mean(axis=1)
-tau_acceptance = output[1][:,:,1].mean(axis=1)
-xi_acceptance = output[1][:,:,2].mean(axis=1)
-
-pd.Series(phi_acceptance).plot()
-pd.Series(tau_acceptance).plot()
-pd.Series(xi_acceptance).plot()
-
-ess = output[2]
+ess = output[1]
 pd.Series(ess).plot()
 
-epsilon = output[3]
+epsilon = output[2]
 pd.Series(epsilon).plot() 
+
+w = np.ones(4)
+a = np.full(10,0)
+np.concatenate(w,a, axis=1)
+